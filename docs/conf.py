1ed92ab5a5a02c50a2081462f461291fcb4f025a
ants-sinica <ants.sinica@gmail.com>
2016-11-25 14:31:58 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 import sys, os
 
@@ -6,40 +7,24 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 sys.path.append(os.path.abspath(os.pardir))
 
-__version__ = '0.9.0'
+__version__ = '1.0'
 
 # -- General configuration -----------------------------------------------------
-templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 project = 'NHIRD-IIS'
 copyright = '2016, IIS, Academia Sinica'
-exclude_patterns = ['_build']
-release = __version__
-version = '.'.join(release.split('.')[:1])
-last_stable = '1.0.0'
-#rst_prolog = '''
-#.. |last_stable| replace:: :nhird-iis-doc:`{0}`
-#'''.format(last_stable)
-
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'python'
 
+extlinks = {}
 
 # -- Options for HTML output ---------------------------------------------------
 
 html_theme = 'default'
-#if not on_rtd:
-#    try:
-#        import sphinx_rtd_theme
-#        html_theme = 'sphinx_rtd_theme'
-#        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-#    except ImportError:
-#        pass
 
 html_static_path = ['static']
 
 def setup(app):
     # overrides for wide tables in RTD theme
-    app.add_stylesheet('theme_overrides.css')   # path relative to _static
-
+    app.add_stylesheet('theme_overrides.css') # path relative to static
